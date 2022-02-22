00860f0e30b105b664c35cb553166c816e2a926d
Nikita <nikich98@yandex.ru>
2022-02-22 19:49:48 +0300

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+cd $WORKDIR
+
+cat python.pid | xargs -n1 kill
+
+source env/bin/activate
+
+pip install -r scripts/srequirements.txt --no-cache-dir
+
+cd $WORKDIR/src
+
+nohup $WORKDIR/env/bin/python -m eventSvc > ../eventSvc.out 2>&1 & echo $! > ../python.pid
+nohup $WORKDIR/env/bin/python -m playerSvc > ../playerSvc.out 2>&1 & echo $! >> ../python.pid
+nohup $WORKDIR/env/bin/python -m eventManagerSvc > ../eventManagerSvc.out 2>&1 & echo $! >> ../python.pid

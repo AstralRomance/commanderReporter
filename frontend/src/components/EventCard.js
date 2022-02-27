ca9b7b586246ede1d4b0b726e108bca142dc5e11
AstralRomance <andrdoncwork@mail.ru>
2022-02-27 22:41:01 +0300

    frontend experiments

---
+++ frontend/src/components/EventCard.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class EventCard extends Component{
+    constructor(props ){
+        super(props)
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
+    };
+
+    componentDidMount()
+    {
+        fetch("http://localhost:8002/events/", {mode: 'no-cors'})
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(result)
+                this.setState(
+                    {
+                        isLoaded: true,
+                        items: result
+                    });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render () {
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <h2>Error {error.message}</h2>
+        } else if (!isLoaded) {
+            return <h2>Loading</h2>
+        } else {
+            return  (
+                <ul>
+                    {items.map(item => {
+                        <li key={item.Event_id}>
+                            {item.Event_name}
+                        </li>
+                    })}
+                </ul>
+            )
+        }
+    }
+}

f7c5df84480c99ee608ff2145b357abcb263d8a8
AstralRomance <andrdoncwork@mail.ru>
2022-03-22 20:39:53 +0300

    Add grid, add link generation to frontend.

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -38,21 +38,25 @@ export default class EventCard extends Component{
         }
         else {
             return (
-                <div class="row">
-                    <div class="col s10 m3">
-                            {items.map(item => {return (
-                                <div class="card blue-grey darken-1">
-                                    <div class="card-content white-text">
-                                        <span class="card-title">
-                                            <h4>{item.Event_name}</h4>
-                                            <p>{item.Event_Date}</p>
-                                        </span>
+                <div className="container">
+                    <h1 align="center">There is all events</h1>
+                    <div class="row">
+                                {items.map(item => {return (
+                                    <div class="col s4">
+                                        <div class="card blue-grey darken-1">
+                                            <div class="card-content white-text">
+                                                <span class="card-title">
+                                                    <h4>{item.Event_name}</h4>
+                                                    <p>{item.Event_Date}</p>
+                                                </span>
+                                            </div>
+                                            <div class="card-action">
+                                                {/* Good way is rework it using NavLink */}
+                                                <a href={`http://localhost:3000/events/${item.Event_id}`}>Go to event {'>'}</a>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="card-action">
-                                        <a href="#">Link to event page</a>
-                                    </div>
-                                </div>
-                        )})}
+                            )})}
                     </div>
                 </div>
             )

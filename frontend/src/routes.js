29519d6218369d43c8ff3cdfa3bd0f10acf5dfa8
NikitaCartes <nikich98@yandex.ru>
2022-03-27 16:11:30 +0300

    Update

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,6 +6,7 @@ import { Routes,
 import { AllEventsPage } from './pages/AllEventsPage'
 import { EventInfo } from './components/EventInfo'
 import { CreateEvent } from './components/CreateEvent'
+import { Json } from './components/Json/Json'
 
 
 export const useRoutes = () => {
@@ -15,6 +16,7 @@ export const useRoutes = () => {
                 <Route path="/event" element={<EventInfo />} />
                 <Route path="/create-event" element={<CreateEvent />} />
                 <Route path="/" element={<Navigate replace to="/events" />} />
+                <Route path="/json" element={<Json/>} />
             </Routes>
     )
 }

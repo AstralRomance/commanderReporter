453c425e217a2c1b2b40373d9c763df09efa1551
AstralRomance <andrdoncwork@mail.ru>
2022-04-03 23:24:05 +0300

    Updated gen_points for new schema

--- src/eventManagerSvc/api/eventManagerSvc.py
+++ src/eventManagerSvc/api/eventManagerSvc.py
@@ -25,7 +25,10 @@ def remove_player_from_event(event_id: str, player_id: str, manager_svc: EventMa
 @router.put('/update-player-points/{event_id}/{player_id}')
 def update_player_points(event_id: str, player_id: str, round_num: int, player_data: UpdatePlayerPoints,
                          manager_svc: EventManagerSvc = Depends()):
-    return manager_svc.update_player_points(event_id, player_id, round_num, player_data)
+    update_response = manager_svc.update_player_points(event_id, player_id, round_num, player_data)
+    if not update_response:
+        return 404
+    return update_response
 
 
 @router.put('/generate-round/{event_id}')

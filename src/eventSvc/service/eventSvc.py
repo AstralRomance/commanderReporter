ef72573697a8ebf084885879a01c72b55cf6be9a
AstralRomance <andrdoncwork@mail.ru>
2022-04-13 19:17:55 +0300

    Date parsing when create event

--- src/eventSvc/service/eventSvc.py
+++ src/eventSvc/service/eventSvc.py
@@ -1,6 +1,7 @@
 from typing import List
 from uuid import uuid4
 import uuid
+from datetime import datetime
 
 from databaseSvc.databaseManipulation import DataBaseManipulation
 from fastapi import HTTPException, Depends
@@ -26,6 +27,7 @@ class EventService:
         actual_event_data['Status'] = 'created'
         actual_event_data['Event_id'] = str(uuid4())
         actual_event_data['Rounds'] = []
+        actual_event_data['Event_Date'] = datetime.strptime(actual_event_data['Event_Date'], '%d %B, %Y')
         self.session.insert_event(actual_event_data)
         return actual_event_data
 

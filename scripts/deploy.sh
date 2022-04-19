715e92a0be3d633492f7a24bac6f1c452c65a041
Nikita <nikich98@yandex.ru>
2022-04-19 16:58:05 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,6 @@ pip install -r scripts/requirements.txt --no-cache-dir
 
 cd $WORKDIR/src
 
-nohup $WORKDIR/env/bin/python -m eventSvc > ../eventSvc.out 2>&1 & echo $! > ../python.pid
-nohup $WORKDIR/env/bin/python -m playerSvc > ../playerSvc.out 2>&1 & echo $! >> ../python.pid
-nohup $WORKDIR/env/bin/python -m eventManagerSvc > ../eventManagerSvc.out 2>&1 & echo $! >> ../python.pid
+nohup $WORKDIR/env/bin/python -m eventSvc > ../logs/eventSvc.out 2>&1 & echo $! > ../python.pid
+nohup $WORKDIR/env/bin/python -m playerSvc > ../logs/playerSvc.out 2>&1 & echo $! >> ../python.pid
+nohup $WORKDIR/env/bin/python -m eventManagerSvc > ../logs/eventManagerSvc.out 2>&1 & echo $! >> ../python.pid

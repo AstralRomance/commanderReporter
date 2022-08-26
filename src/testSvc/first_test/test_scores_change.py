ae4dbe57e9b86b0252de83726f500047d098010c
sunshineinabagg <sunshineinabagg@yandex.ru>
2022-08-26 15:30:48 +0300

    Task #36 update
    Added rounds generation

--- src/testSvc/first_test/test_scores_change.py
+++ src/testSvc/first_test/test_scores_change.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import random
 import requests
 from constants import DEFAULT_TARGET_ENV
 
@@ -8,15 +9,22 @@ def test_scores_change(setup_database_connection, generate_workflow_target_event
     event_data = generate_workflow_target_event
     mongo_session = setup_database_connection
     target_endpoint = f'{DEFAULT_TARGET_ENV}/event-manager/add-player/{event_data["Event_id"]}'
-    player_info = add_player()
-    resp = requests.post(target_endpoint, data = json.dumps(player_info))
+    player_name, player_commander = add_player()
+    resp = requests.post(target_endpoint, json = {'Player_name': player_name, 'Commander': player_commander})
     assert resp.ok
-    db_event_data = mongo_session.find_one({'Event_id': event_data['Event_id']})
+    rounds_num = random.randint(1, 5)
+    round_generate_endpoint = f'{DEFAULT_TARGET_ENV}/event-manager/generate-round/{event_data["Event_id"]}'
+    for i in range(rounds_num):
+        round_gen_response = requests.put(round_generate_endpoint, params={'round_number': i})
+        assert round_gen_response.ok
+        update_points_endpoint = f'{DEFAULT_TARGET_ENV}/event-manager/update-player-points/{event_data["Event_id"]}/{resp.json().get("Player_id")}'
+        update_points_response = requests.put(update_points_endpoint,
+                                              params={'round_num': i},
+                                              json={'Points': 3,
+                                                    'Sub_points': 4})
+    
     player_db_data = mongo_session.find_one({'Event_id': event_data["Event_id"]},
-                                            {'Players': {'$elemMatch': {'Player_id': ['id']}}}).get('Players')[0]
-    update_points_endpoint = f'{DEFAULT_TARGET_ENV}/event-manager/update-player-points/{event_data["Event_id"]}/'
-    update_points_response = requests.put(update_points_endpoint+['id'],
-                                          data=json.dumps({'Points': 3,
-                                                           'Sub_points': 4}))
-    assert player_db_data['Points'] == 3
-    assert player_db_data['Sub_points'] == 4
+                                            {'Players': {'$elemMatch': {'Player_id': resp.json().get('Player_id')}}}).get('Players')[0]
+    assert player_db_data['Points'] == 3 * rounds_num
+    assert player_db_data['Sub_points'] == 4 * rounds_num
+    
